53a21bc73fd3af97c6ef845b77d4969fb1076dd1
Kris <kris@lab101.be>
2024-10-11 00:18:28 +0200

    grids

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,34 @@
 
 
+
+class HitBox {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        this.innerX = 0;
+        this.innerY = 0;
+        this.innerW = 0;
+        this.innerH = 0;
+     
+
+        this.counter = 0;
+    }
+
+    setInner(){
+        this.innerX = this.x + (this.w * 0.25);
+        this.innerY = this.y + (this.h * 0.25);
+        this.innerW = this.w * 0.5;
+        this.innerH = this.h * 0.5;
+    }
+
+    contains(x, y) {
+        return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h;
+    }
+}
+
 var websocket = 0;
 var timer = 0;
 
@@ -8,31 +37,96 @@ var websocketmanager = new WebSocketManager(socketUrl);
 // particle array
 var particles = [];
 
+var grids = [];
+
 var gridImage;
+
+
+
 
 
 function preload() {
     gridImage = loadImage('grid.png');
 }
 
+function resize() {
+    resizeCanvas(windowWidth, windowHeight);
+    console.log("resized");
+    //grids = [];
+    let i = 0;
+    for(let row = 0; row < 9; row++){
+        for(let col = 0; col < 7; col++){
+            let x = col*innerWidth/7 + (innerWidth/7 * 0.17);
+            let y = row*innerHeight/9 + (innerHeight/9 * 0.21);
+            let width = innerWidth/7 * 0.83;
+            let height = innerHeight/9 * 0.83;
+            var hitBox = new HitBox(x,y,width,height);
+            
+            grids[i].x = x;
+            grids[i].y = y;
+            grids[i].w = width;
+            grids[i].h = height;
+
+            grids[i].setInner();
+            i++;
+            
+            //grids.push(hitBox);
+        }
+    }
+}    
+
 function setup() {
 
+    // document resize
+window.addEventListener('resize', function(event){
+    resize();
+
+});
     // get the canvas by its id
     var canvas = document.getElementById("canvas");
     createCanvas(canvas.clientWidth, canvas.clientHeight, canvas);
     background(0);
 
+    for(let row = 0; row < 9; row++){
+        for(let col = 0; col < 7; col++){
+            let x = col*innerWidth/7 + (innerWidth/7 * 0.17);
+            let y = row*innerHeight/9 + (innerHeight/9 * 0.21);
+            let width = innerWidth/7 * 0.83;
+            let height = innerHeight/9 * 0.83;
+            var hitBox = new HitBox(x,y,width,height);
+            hitBox.setInner();
+            grids.push(hitBox);
+        }
+    }
+
    // gridImage.src = "/grid.png";
+}
+
+function checkIfInGrid(x,y){
+    for(let i = 0; i < grids.length; i++){
+        if(grids[i].contains(x,y)){
+            return i;
+        }
+    }
+    return -1;
 }
 
 function draw() {
     background(0);
 // draw the grid image
+
+    // get parameters from the url
+    var url = new URL(window.location.href);
+    var debug = url.searchParams.get("debug");
+    if(debug){
+        
+
         if(gridImage){
             fill(255);
            // console.log("draw grid");
             image(gridImage,0,0,innerWidth,innerHeight);
         }
+    }
     // own circle
     if (mouseIsPressed) {
         stroke(255, 0, 255);
@@ -45,6 +139,12 @@ function draw() {
         circle(gridX, gridY, 20);
         websocketmanager.send(gridX / innerWidth, gridY / innerHeight)
 
+        var gridIndex = checkIfInGrid(gridX,gridY);
+        if(gridIndex != -1){
+            if(grids[gridIndex].counter < 20){
+                grids[gridIndex].counter++;
+            }
+        }
 
     }
 
@@ -52,11 +152,23 @@ function draw() {
     for (var i = 0; i < websocketmanager.users.length; i++) {
         var user = websocketmanager.users[i];
         var time = user.getTimeDiv();
+
+        if (time > 100) {
+            continue;
+        }
         var radius = 50 - time / 100;
         if (radius < 0) radius = 10;
 
         var screenX = innerWidth * user.x;
         var screenY = innerHeight * user.y;
+
+        var gridIndex = checkIfInGrid(screenX,screenY);
+        if(gridIndex != -1 ){
+            if(grids[gridIndex].counter < 20){
+                grids[gridIndex].counter++;
+            }
+
+        }
 
         colorMode(HSB, 100);
         var id = user.id;
@@ -87,13 +199,55 @@ function draw() {
         var lifetime = particles[i].lifeTime;
         var saturation = map(lifetime,0,100,0,80);
         saturation = Math.min(saturation,80);
-        var radius = map(lifetime,0,100,0,7);
-        radius = Math.min(radius,7);
+        var radius = map(lifetime,0,100,0,2);
+        radius = Math.min(radius,2);
 
-        fill(particles[i].hue,saturation,100,particles[i].lifeTime/400*100);
+        //fill(particles[i].hue,saturation,100,particles[i].lifeTime/400*100);
+        fill(255,0,255);
         circle(particles[i].x,particles[i].y,radius);
     }
     noFill();
+
+    // draw the grid
+    for(let i = 0; i < grids.length; i++){
+        stroke(255);
+
+        if(grids[i].counter > 0){
+           grids[i].counter-=0.02;
+        }
+
+        if(grids[i].counter > 0){
+            var grid = grids[i];
+            let w = map(grids[i].counter,0,40,1,15);
+            if(w > 15){
+                w = 15;
+            }
+            strokeWeight(w);
+    
+            let c = map(grids[i].counter,0,100,40,255);
+            stroke(c);
+
+            rect(grid.innerX,grid.innerY,grid.innerW,grid.innerH);
+
+            if(grid.counter > 10){
+
+                let w = map(grids[i].counter,0,30,0.5,2);
+                if(w > 2){
+                    w = 2;
+                }
+                strokeWeight(w);
+                let c = map(grids[i].counter,0,40,40,255);
+                stroke(c);
+    
+                rect(grid.x,grid.y,grid.w,grid.h);
+
+            }
+            // draw text
+            strokeWeight(1);
+           // text(grid.counter,grid.x+grid.w/2,grid.y+grid.h/2);
+        }
+
+    }
 
     //remove dead particles
     for(var i = 0; i < particles.length; i++){
